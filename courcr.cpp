0a8dced3066871482fd6b7348893579e88f19d91
p33 <p33@step.lv>
2024-03-06 18:38:53 +0200

    add cour

---
+++ courcr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+bool fuc()
+{
+  return 1;
+}
+
+using namespace std;
+
+void main()
+{
+  
+  int a = 5;
+  char b = 'a';
+
+  cout << "hello" << endl;
+  cout << "hi" << endl;
+}
